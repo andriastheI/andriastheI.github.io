b75749d952d041c8f228c23287c348da18a40377
andriastheI <azelele@carroll.edu>
2024-04-02 07:24:49 -0600

    javascript lessons

--- practice/Learning/JS_Youtube.js
+++ practice/Learning/JS_Youtube.js
@@ -1,1 +1,70 @@
 console.log("Hello World");
+
+
+
+
+// setting variabels 
+var myName = "ILoveYou"
+
+// all lines in a javascript ends with a ;(semicolon) 
+// declaring and assigning a variable
+var a; // declaring a variable
+var b = 2; // assigning(initializing) and declaring a variable
+
+// we can change the variable value 
+
+a = 123; //it is already declared we can change the value 
+  
+b = a; // we can assign its value to another variable 
+
+console.log("") //lets us display any content in the console 
+
+console.log(a)
+
+// You can manipulate numbers using the 4 
+// symbols of math on the declaration or initialization of variables
+
+var c = 121 - 21;
+
+// You can increment and decrement a number by one as follows
+c++; //increment(increase by one)
+c--; //decrement(decrease by one)
+
+// an escaping character \(backslash) for using quotation in a quotations(before the quote we want it to read).
+// or we can use single quotes and backstring(``)
+// most coding forms are the same as python coding 
+// .length to a variable will return the length of the string 
+// [i] indexing is the same as python it starts at 0
+
+
+function wordPuzzle(verb,adjective,noun) {
+    var result = "";
+    result += "The " + noun + " is " + verb + "ing " + adjective + ".";
+
+    return result;
+}
+
+console.log(wordPuzzle("bark","loud","dog"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
